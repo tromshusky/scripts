d503d1e69f15a0332280a9349e75b7c9c81726cc
tromshusky <tromshusky@gmail.com>
2024-05-27 21:04:19 +0200

    Update batt.sh

--- batt.sh
+++ batt.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-#calculates the battery percentage x100 and then replaces one (the first) character with itself and a dot
-echo $((10000 * $(cat /sys/class/power_supply/*battery/charge_now) / $(cat /sys/class/power_supply/*battery/charge_full)))% | sed 's|.|&.|'
+#calculates the battery percentage x100 and then divides by 100 with awk, so it shows as decimal
+echo $((10000 * $(cat /sys/class/power_supply/*battery/charge_now) / $(cat /sys/class/power_supply/*battery/charge_full))) | awk '{print $1 / 100 "%"}'
