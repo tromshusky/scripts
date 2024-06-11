209c143ef246a8eb6f927ec72407d3ee5c9b21b0
tromshusky <tromshusky@gmail.com>
2024-06-11 19:26:44 +0000

    Update move-to-nix-store.sh

--- move-to-nix-store.sh
+++ move-to-nix-store.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+true ${1?} || exit
 mktemp -u && nix-store --version &&  mv --help || echo depends on mktemp nix-store mv
 mktemp -u && nix-store --version &&  mv --help || exit
 
-mv $(nix-store --realise $(nix-store --add $1) --add-root $(mktemp -u)) $1
+mv $(nix-store --realise $(nix-store --add $1) --add-root $(mktemp -u)) ${2-$1}
