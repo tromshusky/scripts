6c12240b04144324dda855a1ad27166348c3ec6d
tromshusky <tromshusky@gmail.com>
2024-06-11 19:29:30 +0000

    Create bash-argc.sh

---
+++ bash-argc.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo \$\#: $#
+echo \$1: $1
+echo \$2: $2
+echo \$3: $3
+echo \$\{3-\$1\}: ${3-$1}
+echo
+echo exit if less than 2 arguments:
+printf "true ${2?} || exit"
+true ${2?} || exit
