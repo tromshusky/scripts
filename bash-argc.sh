6c2848ed79a0c3d994e9a52a731c0fa2b64efc9f
tromshusky <tromshusky@gmail.com>
2024-06-11 19:34:50 +0000

    Update bash-argc.sh

--- bash-argc.sh
+++ bash-argc.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo \$\#: $#
-echo \$1: $1
-echo \$2: $2
-echo \$3: $3
+echo \$1: ${1+\"}$1${1+\"}
+echo \$2: ${2+\"}$2${2+\"}
+echo \$3: ${3+\"}$3${3+\"}
 echo \$\{3-\$1\}: ${3-$1}
 echo
 echo exit if less than 2 arguments:
